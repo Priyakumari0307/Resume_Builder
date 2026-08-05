d9aa2b850aa403b424c09cd0dc06d7c9a212d1c2
Priya Kumari <kumaripriya934100@gmail.com>
2026-08-05 10:12:15 +0530

    chore: update API base URL to new render backend

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: import.meta.env.VITE_BASE_URL || "https://resume-builder-cu7h.onrender.com",
 })
 
 export default api
